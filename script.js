8413cbbb9b4a3afb4f1dd0a218b983e21a957ce2
jonathanlatalisa <95480314+jonathanlatalisa@users.noreply.github.com>
2022-03-14 12:23:06 -0300

    atualização projeto

---
+++ script.js
@@ -0,0 +1,29 @@
+function toggleVideo() {
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+    trailer.classList.toggle('active');
+
+    video.currentTime = 0;
+    video.pause();
+}
+
+
+var mySwiper = new Swiper('.swiper-container', {
+    spaceBetween: 5,
+    slidesPerView: 2,
+    loop: true,
+    freeMode: true,
+    loopAdditionalSlides: 5,
+    speed: 500,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+            freeMode: false
+        }
+    }
+})
